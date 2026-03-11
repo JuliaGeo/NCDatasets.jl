7967a509f68e080222761387426346f427866b67
Alexander Barth <barth.alexander@gmail.com>
2026-03-11 09:58:41 +0100

    local name2

--- test/test_lowlevel.jl
+++ test/test_lowlevel.jl
@@ -24,7 +24,7 @@ samples = [
 
 
 for sampledata in samples
-    local start, varid, varid, T, dimids, ncid, xtype
+    local start, varid, varid, T, dimids, ncid, xtype, name2
     rm(filename;force=true)
 
     # write data
