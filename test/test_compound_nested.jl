71f58fd9b6178f7f7fd6aa0fb48d0d16ec208fe8
Alexander Barth <barth.alexander@gmail.com>
2026-07-17 14:37:31 +0200

    more tests for test_compound_nested.jl

--- test/test_compound_nested.jl
+++ test/test_compound_nested.jl
@@ -57,3 +57,37 @@ usertype!(ds,"ObsGrid",ObsGrid)
 @test ds["data"][:] == data_ref
 
 close(ds)
+
+
+# deeply nested
+
+struct Position
+    lon::Float32
+    lat::Float32
+end
+
+struct Profile
+    position::Position
+    temperature::NTuple{3,Float32}
+end
+
+struct Entry
+    id::Int32
+    profile::Profile
+end
+
+data_ref = [Entry(404,Profile(Position(1,2),(1,2,3)))]
+
+fname = tempname()
+ds = NCDataset(fname,"c");
+defVar(ds,"data",data_ref,("n",))
+close(ds)
+
+ds = NCDataset(fname);
+data = ds["data"][:]
+@test data[1].id == data_ref[1].id
+@test data[1].profile.position.lon == data_ref[1].profile.position.lon
+
+usertype!(ds,"Entry",Entry)
+data = ds["data"][:]
+@test data == data_ref
