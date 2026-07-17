0818a6414d23fc88e918fe90a32cd2f23e15029b
Alexander Barth <barth.alexander@gmail.com>
2026-07-17 14:01:55 +0200

    vector of user types

--- test/test_compound_nested.jl
+++ test/test_compound_nested.jl
@@ -6,6 +6,7 @@ struct Position
     lon::Float32
     lat::Float32
 end
+
 struct Obs
     temperature::Float32
     humidity::Int32
@@ -32,5 +33,27 @@ data = ds["weather_reports"][:]
 usertype!(ds,"Obs",Obs)
 data = ds["weather_reports"][:]
 @test data == data_ref
+
+close(ds)
+
+
+# vector of user types
+
+struct ObsGrid
+    id::Int32
+    position::NTuple{3,Position}
+end
+
+data_ref = [ObsGrid(42,Position.((1,2,3),(10,20,30)))]
+fname = tempname()
+ds = NCDataset(fname,"c");
+defVar(ds,"data",data_ref,("n",))
+close(ds)
+
+ds = NCDataset(fname);
+@test ds["data"][1].id == 42
+
+usertype!(ds,"ObsGrid",ObsGrid)
+@test ds["data"][:] == data_ref
 
 close(ds)
