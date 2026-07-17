c6477049e239005b5a925429345a0e92748109b5
Alexander Barth <barth.alexander@gmail.com>
2026-07-17 08:38:36 +0200

    nc_inq_var returns the type number similar to nc_inq_vartype

--- src/netcdf_c.jl
+++ src/netcdf_c.jl
@@ -1552,7 +1552,7 @@ function _jltype(ncid,xtype,usertypes)
 end
 
 
-@with_lock function nc_inq_var(ncid::Integer,varid::Integer,usertypes)
+@with_lock function nc_inq_var(ncid::Integer,varid::Integer)
     ndims = nc_inq_varndims(ncid,varid)
 
     ndimsp = Ref(Cint(0))
@@ -1566,9 +1566,8 @@ end
     name = safe_string(cname)
 
     xtype = xtypep[]
-    jltype = _jltype(ncid,xtype,usertypes)
 
-    return name,jltype,dimids,nattsp[]
+    return name,xtype,dimids,nattsp[]
 end
 
 @with_lock function nc_inq_varid(ncid::Integer,name)
